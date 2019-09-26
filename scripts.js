6b401fd4ecbff9bdfbd22e022f53395a195c519a
Alex Forrana <forrana@gmail.com>
2019-09-26 16:43:15 +0300

    make format to more human readable

--- scripts.js
+++ scripts.js
@@ -8,17 +8,18 @@ function calculateCountDown() {
   let currentDate = new Date();
 
   let msLeft = deadlineDate - currentDate;
-  let hoursLeft = msLeft / 1000 / 60 / 60
+  let weeksLeft = msLeft / 1000 / 60 / 60 / 24 / 7
+  let daysLeft = weeksLeft % 1 * 7
+  let hoursLeft = daysLeft % 1 * 24
   let minutesLeft = hoursLeft % 1 * 60
   let secondsLeft = minutesLeft % 1 * 60
 
-  let daysLeft = Math.round(hoursLeft / 24)
-  let weeksLeft =Math.round(daysLeft / 7)
   let secondsLeftStr = ("" + Math.round(secondsLeft)).padStart(2, "0")
   let minutesLeftStr = ("" + Math.round(minutesLeft)).padStart(2, "0")
   let hoursLeftStr =  ("" + Math.round(hoursLeft)).padStart(2, "0")
-  countdownTimer.innerHTML = `${hoursLeftStr}:${minutesLeftStr}:${secondsLeftStr}`
-  countdownDater.innerHTML = `${weeksLeft} <small>weeks</small>, ${daysLeft} <small>days</small>`
+  let daysLeftStr = Math.round(daysLeft)
+  let weeksLeftStr = Math.round(weeksLeft)
+  countdownTimer.innerHTML = `${weeksLeftStr} <small>weeks</small>, ${daysLeftStr} <small>days</small> ${hoursLeftStr}:${minutesLeftStr}:${secondsLeftStr}`
 }
 
 calculateCountDown()
